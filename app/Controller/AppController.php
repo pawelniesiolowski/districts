11b33a8775fc2100eaba856c997029f3722b45ff
pawelniesiolowski <pavel.niesiolowski@gmail.com>
2018-06-29 21:35:16 +0200

    feat: Create AppController

---
+++ app/Controller/AppController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Districts\Controller;
+
+
+class AppController implements ControllerInterface
+{
+    public function displayMainPage()
+    {
+        echo "Hello world!";
+    }
+}
---
+++ app/Controller/ControllerInterface.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Districts\Controller;
+
+
+interface ControllerInterface {}
