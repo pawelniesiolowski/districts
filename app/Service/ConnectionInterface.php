a1e94951f35ed7938214400d3a1a043a78f26ea4
pawelniesiolowski <pavel.niesiolowski@gmail.com>
2018-06-29 21:36:41 +0200

    feat: Create PDOConnection

---
+++ app/Service/ConnectionInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Districts\Service;
+
+
+interface ConnectionInterface
+{
+    public function getConnection();
+}
---
+++ app/Service/PDOConnection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Districts\Service;
+
+
+class PDOConnection implements ConnectionInterface
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+        try {
+            $this->pdo = new \PDO($dsn, DB_USER, DB_PASS, [
+                \PDO::ATTR_EMULATE_PREPARES => false,
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+            ]);
+            $this->pdo->query('SET NAMES "utf8"');
+        } catch (\PDOException $e) {
+            exit($e->getMessage() . PHP_EOL);
+        }
+    }
+
+    public function getConnection(): \PDO
+    {
+        return $this->pdo;
+    }
+}
