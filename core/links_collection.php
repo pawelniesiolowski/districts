4984f9e7e7fae794061914b6a3b0b6156001dce2
pawelniesiolowski <pavel.niesiolowski@gmail.com>
2018-06-30 18:41:31 +0200

    Create paths for new actions

--- core/links_collection.php
+++ core/links_collection.php
@@ -2,6 +2,8 @@
 
 $linksCollection = new \Districts\Router\LinksCollection();
 
-$linksCollection->add('/', new \Districts\Router\Link('AppController', 'displayMainPage'));
+$linksCollection->add('', new \Districts\Router\Link('AppController', 'displayMainPage', ['sort' => '/[A-Za-z_.]/'], false));
+
+$linksCollection->add('delete', new \Districts\Router\Link('AppController', 'delete', ['id' => '/^\d+$/', 'city' => '/[A-Za-z_.]/'], true));
 
 return $linksCollection;
