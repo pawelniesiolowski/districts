727aa83abe6a2063467a09afba3e7c76d90e0814
pawelniesiolowski <pavel.niesiolowski@gmail.com>
2018-06-30 11:30:35 +0200

    chore: Change database tables names for more clarity

--- sql.sql
+++ sql.sql
@@ -4,14 +4,14 @@ CREATE DATABASE districts
 
 CREATE TABLE city (
     city_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name varchar(50) NOT NULL
+    city_name varchar(50) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE utf8_polish_ci;
 
 CREATE TABLE district (
     district_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name varchar(50) NOT NULL,
-    population int NOT NULL,
-    area float NOT NULL,
+    district_name varchar(50) NOT NULL,
+    district_population int NOT NULL,
+    district_area float NOT NULL,
     city_id int NOT NULL,
 
     CONSTRAINT district_city_fk
