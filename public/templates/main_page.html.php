44a0b2805ece5b7a107cf9a188f70d8642e4e6ea
pawelniesiolowski <pavel.niesiolowski@gmail.com>
2018-06-30 18:41:56 +0200

    Create basic view

---
+++ public/templates/main_page.html.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Districts Application</title>
+    <link rel="stylesheet" href="./../css/styles.css">
+</head>
+<body>
+    <h1>Districts Application</h1>
+    <h3>Informacje na temat dzielnic Gdańska i Krakowa</h3>
+    <table id="district_table">
+        <tr>
+            <th><a href="<?= './?sort=name' ?>">Nazwa</a></th>
+            <th><a href="<?= './?sort=population' ?>">Populacja</a></th>
+            <th><a href="<?= './?sort=area' ?>">Powierzchnia</a></th>
+            <th><a href="<?= './?sort=city_name' ?>">Miasto</a></th>
+            <th>Usuń</th>
+        </tr>
+        <?php foreach ($districts as $district): ?>
+        <tr>
+            <td><?= $district->name ?></td>
+            <td><?= $district->population ?></td>
+            <td><?= $district->area ?></td>
+            <td><?= $district->city ?></td>
+            <td><a href="./delete?id=<?= $district->id ?>">X</a></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
