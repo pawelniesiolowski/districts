6273175abdee527bd8fd740d8fb55161d3f4cffe
pawelniesiolowski <pavel.niesiolowski@gmail.com>
2018-07-08 03:01:25 +0200

    Use decimal type for area to easy compare values

--- sql.sql
+++ sql.sql
@@ -6,6 +6,6 @@ CREATE TABLE district (
     district_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name varchar(50) NOT NULL,
     population int NOT NULL,
-    area float NOT NULL,
+    area decimal(5,2) NOT NULL,
     city varchar(50) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE utf8_polish_ci;
